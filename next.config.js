51b4711608b46610e093cb4011f140d8945d0892
youssef-degachi <youssefdegachi0@gmail.com>
2024-11-07 13:48:56 +0100

    add next.config and modfiy deployment of workflows

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+module.exports = {
+  // Optional: Add basePath if using a sub-path on GitHub Pages
+  basePath: '/my_prortfolio',
+  assetPrefix: '/my_prortfolio',
+  trailingSlash: true,  // Ensure trailing slash is added to the URLs
+};
